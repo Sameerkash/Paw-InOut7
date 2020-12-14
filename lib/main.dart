b23fbad871710c95efe08aa394a4df9b9d3445c8
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-12-14 14:28:15 +0530

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Paw',
       theme: ThemeData(
         primarySwatch: Colors.blue,
